461f42d2735e79905c36a990ac2cba09b66c655d
pato694 <pato694@seznam.cz>
2020-11-10 09:53:51 +0100

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,85 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "esp_camera.h"
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+#include <Wire.h>
+#include "SparkFunBME280.h"
+
+// Replace with your network credentials
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+String chatId = "XXXXXXXXXX";
+
+// Initialize Telegram BOT
+String BOTtoken = "XXXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+bool sendPhoto = false;
+
+WiFiClientSecure clientTCP;
+
+UniversalTelegramBot bot(BOTtoken, clientTCP);
+
+//CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+#define FLASH_LED_PIN 4
+bool flashState = LOW;
+
+// Motion Sensor
+bool motionDetected = false;
+
+// Define I2C Pins for BME280
+#define I2C_SDA 14
+#define I2C_SCL 15
+
+BME280 bme;
+ 
+int botRequestDelay = 1000;   // mean time between scan messages
+long lastTimeBotRan;     // last time messages' scan has been done
+
+void handleNewMessages(int numNewMessages);
+String sendPhotoTelegram();
+
+// Get BME280 sensor readings and return them as a String variable
+String getReadings(){
+  float temperature, humidity;
+  temperature = bme.readTempC();
+  //temperature = bme.readTempF();
+  humidity = bme.readFloatHumidity();
+  String message = "Temperature: " + String(temperature) + " ºC \n";
+  message += "Humidity: " + String (humidity) + " % \n";
+  return message;
+}
+
+// Indicates when motion is detected
+static void IRAM_ATTR detectsMovement(void * arg){
+  //Serial.println("MOTION DETECTED!!!");
+  motionDetected = true;
+}
 
 void setup() {
   // put your setup code here, to run once:
